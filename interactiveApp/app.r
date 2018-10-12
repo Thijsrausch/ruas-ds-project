0a52b5af99108230d8de588c092831109ea05dbc
Menno van Rijsselberg <0919433@hr.nl>
2018-10-12 10:52:37 +0200

    App now takes .csv files

--- interactiveApp/app.r
+++ interactiveApp/app.r
@@ -1,3 +1,4 @@
-library(jsonlite)
-jsonData <- fromJSON("ExampleAirBnBJson.json")
+//Test data from insideairbnb.com/get-the-data.html
+jsonData <- read.csv(file = "listingsSep18.csv", header = TRUE, sep = ",")
 str(jsonData)
+uniqueValues <- unique(jsonData$neighbourhood)
