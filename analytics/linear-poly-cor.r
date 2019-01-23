e5e049ef295de95310b9d08fa6a6327c9f6c8ba9
Thijs Rausch <thijsrausch@gmail.com>
2019-01-23 15:45:19 +0100

    add a model based on einstein functions to fit distance to center data

--- analytics/linear-poly-cor.r
+++ analytics/linear-poly-cor.r
@@ -18,3 +18,22 @@ lines(scrape_rotterdam$bedrooms, predict(pm))
 
 summary(pm)
 summary(lm(price~bedrooms, scrape_rotterdam))
+
+#########
+dtc <- function(x,y) {
+  sqrt((51.9225 - x)^2 + (4.47917 - y)^2)
+} 
+
+logModel <- lm(
+  price ~ I(((dtc(latitude,longitude)/((exp(1)^dtc(latitude,longitude))-1))-log(1-(exp(1)^(-dtc(latitude,longitude)))))),
+  data=scrape_rotterdam)
+
+plot(sqrt((51.9225 - scrape_rotterdam$latitude)^2 + (4.47917 - scrape_rotterdam$longitude)^2),scrape_rotterdam$price)
+lines(sqrt((51.9225 - scrape_rotterdam$latitude)^2 + (4.47917 - scrape_rotterdam$longitude)^2), 
+      predict(logModel, data=scrape_rotterdam))
+summary(logModel)
+
+dtcModel <- lm(price ~ dtc(latitude,longitude), data=scrape_rotterdam)
+lines(sqrt((51.9225 - scrape_rotterdam$latitude)^2 + (4.47917 - scrape_rotterdam$longitude)^2), 
+      predict(dtcModel, data=scrape_rotterdam), col='blue')
+summary(dtcModel)
